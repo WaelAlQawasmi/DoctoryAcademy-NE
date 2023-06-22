62ba519aa625fd89eac80e139acf7af649e26739
waelalq <wael.abuomr@gmail.com>
2023-06-22 10:20:53 +0200

    bulid batabase

---
+++ app/Http/Controllers/MediaCenterController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MediaCenter;
+use Illuminate\Http\Request;
+
+class MediaCenterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MediaCenter $mediaCenter)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MediaCenter $mediaCenter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MediaCenter $mediaCenter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MediaCenter $mediaCenter)
+    {
+        //
+    }
+}
