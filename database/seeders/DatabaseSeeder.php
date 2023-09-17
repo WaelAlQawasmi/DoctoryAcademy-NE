3968e344312945a77960430f488dab285fe5632b
waelalq <wael.abuomr@gmail.com>
2023-09-17 23:21:53 +0300

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([
+            PermissionTableSeeder::class,
+            CreateAdminUserSeeder::class,
+           
+        ]);
+    
 
         
     }
